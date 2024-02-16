14f287074be0de11f9dffc3bab47054a9aa12bc5
Coop <73203995+coopeeo@users.noreply.github.com>
2024-02-15 19:51:31 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include <iostream>
-#include <list>
+#include <vector>
 #include <string>
 
 using namespace geode::prelude;
@@ -11,7 +11,7 @@ class $modify(MenuLayer) {
 	bool init() {
 		if (!MenuLayer::init()) return false;
 
-		std::list<CCSprite*> theChosenList {};
+		std::vector<CCSprite*> theChosenList {};
 
 		auto liteSprite = CCSprite::createWithSpriteFrameName("GJ_lite_001.png");
 		auto meltdownSprite = CCSprite::createWithSpriteFrameName("GJ_md_001.png");
@@ -30,66 +30,122 @@ class $modify(MenuLayer) {
 		auto mgMenu = this->getChildByID("more-games-menu");
 		auto rightSideMenu = this->getChildByID("right-side-menu");
 
-		if (Mod::get()->getSettingValue<bool>("subtext-lite")) {
+		switch (Mod::get()->getSettingValue<int64_t>("subtext-type"))
+		{
+		case 0:
+			// none
+			break;
+		case 1:
+			// lite
 			// liteSprite->setPosition({100, 100});
 			liteSprite->setPositionX(titlePos.x + 150.5);
 			liteSprite->setPositionY(titlePos.y - 30);
 			liteSprite->setScale(0.8);
 			liteSprite->setZOrder(10);
+			this->addChild(liteSprite);
 			liteSprite->setID("lite-title");
-			theChosenList.emplace_back(liteSprite);
-			//liteSprite->setID("lite-title");
-		}
-
-		if (Mod::get()->getSettingValue<bool>("subtext-meltdown")) {
-			/// meltdownSprite->setPosition({ 100, 100 });
+			break;
+		case 2:
+			// meltdown
+			// meltdownSprite->setPosition({ 100, 100 });
 			meltdownSprite->setPositionX(titlePos.x + 140);
 			meltdownSprite->setPositionY(titlePos.y - 44);
 			meltdownSprite->setZOrder(10);
+			this->addChild(meltdownSprite);
 			meltdownSprite->setID("meltdown-title");
-			theChosenList.emplace_back(meltdownSprite);
-			//meltdownSprite->setID("meltdown-title");
-		}
-
-		if (Mod::get()->getSettingValue<bool>("subtext-world")) {
+			break;
+		case 3:
+			// world
 			// worldSprite->setPosition({ 100, 100 });
 			worldSprite->setPositionX(titlePos.x + 120);
 			worldSprite->setPositionY(titlePos.y - 44);
 			worldSprite->setZOrder(10);
+			this->addChild(worldSprite);
 			worldSprite->setID("world-title");
-			theChosenList.emplace_back(worldSprite);
-			//worldSprite->setID("world-title");
-		}
-
-		if (Mod::get()->getSettingValue<bool>("subtext-subzero")) {
+			break;
+		case 4:
+			// subzero
 			// subzeroSprite->setPosition({ 100, 100 });
 			subzeroSprite->setPositionX(titlePos.x + 120);
 			subzeroSprite->setPositionY(titlePos.y - 44);
 			subzeroSprite->setZOrder(10);
+			this->addChild(subzeroSprite);
 			subzeroSprite->setID("subzero-title");
-			theChosenList.emplace_back(subzeroSprite);
-			//subzeroSprite->setID("subzero-title");
-		}
-
-		if (Mod::get()->getSettingValue<bool>("subtext-2point2")) {
+			break;
+		case 5:
+			// 2.2
 			// twopointtwoSprite->setPosition({ 100, 100 });
 			twopointtwoSprite->setPositionX(titlePos.x + 120);
 			twopointtwoSprite->setPositionY(titlePos.y - 44);
 			twopointtwoSprite->setZOrder(10);
+			this->addChild(twopointtwoSprite);
 			twopointtwoSprite->setID("twopointtwo-title");
-			theChosenList.emplace_back(twopointtwoSprite);
-			//twopointtwoSprite->setID("twopointtwo-title");
+			break;
+		case -1:
+			if (Mod::get()->getSettingValue<bool>("subtext-lite")) {
+				// liteSprite->setPosition({100, 100});
+				liteSprite->setPositionX(titlePos.x + 150.5);
+				liteSprite->setPositionY(titlePos.y - 30);
+				liteSprite->setScale(0.8);
+				liteSprite->setZOrder(10);
+				liteSprite->setID("lite-title");
+				theChosenList.emplace_back(liteSprite);
+				//liteSprite->setID("lite-title");
+			}
+	
+			if (Mod::get()->getSettingValue<bool>("subtext-meltdown")) {
+				/// meltdownSprite->setPosition({ 100, 100 });
+				meltdownSprite->setPositionX(titlePos.x + 140);
+				meltdownSprite->setPositionY(titlePos.y - 44);
+				meltdownSprite->setZOrder(10);
+				meltdownSprite->setID("meltdown-title");
+				theChosenList.emplace_back(meltdownSprite);
+				//meltdownSprite->setID("meltdown-title");
+			}
+	
+			if (Mod::get()->getSettingValue<bool>("subtext-world")) {
+				// worldSprite->setPosition({ 100, 100 });
+				worldSprite->setPositionX(titlePos.x + 120);
+				worldSprite->setPositionY(titlePos.y - 44);
+				worldSprite->setZOrder(10);
+				worldSprite->setID("world-title");
+				theChosenList.emplace_back(worldSprite);
+				//worldSprite->setID("world-title");
+			}
+	
+			if (Mod::get()->getSettingValue<bool>("subtext-subzero")) {
+				// subzeroSprite->setPosition({ 100, 100 });
+				subzeroSprite->setPositionX(titlePos.x + 120);
+				subzeroSprite->setPositionY(titlePos.y - 44);
+				subzeroSprite->setZOrder(10);
+				subzeroSprite->setID("subzero-title");
+				theChosenList.emplace_back(subzeroSprite);
+				//subzeroSprite->setID("subzero-title");
+			}
+	
+			if (Mod::get()->getSettingValue<bool>("subtext-2point2")) {
+				// twopointtwoSprite->setPosition({ 100, 100 });
+				twopointtwoSprite->setPositionX(titlePos.x + 120);
+				twopointtwoSprite->setPositionY(titlePos.y - 44);
+				twopointtwoSprite->setZOrder(10);
+				twopointtwoSprite->setID("twopointtwo-title");
+				theChosenList.emplace_back(twopointtwoSprite);
+				//twopointtwoSprite->setID("twopointtwo-title");
+			}
+			std::string result;
+			auto theCount = 0;
+	    		for (const auto& element : theChosenList) {
+				theCount = theCount + 1;
+				if (theCount == 1 || theCount == 0) result += element->getID() + " "; else result += element->getID() + ", ";
+	    		}
+			log::info("The selected texts: {}",result);
+	
+			// todo: have a random thingy selected choose one and make id efewhaufioea->setID("spinoff-title");
+			if (theCount == 0) break;
+			// if (theCount == 1) break;
+			this->addChild(theChosenList[rand() % theCount])
+			break;
 		}
-		std::string result;
-		auto theCount = 0;
-    		for (const auto& element : theChosenList) {
-			theCount = theCount + 1;
-        		result += element->getID() + " ";
-    		}
-		log::info("The selected texts: {}",result);
-
-		// todo: have a random thingy selected choose one and make id efewhaufioea->setID("spinoff-title");
-		switch(rand() % theCount) {}; // todo: finish lol
 		
 		if (Mod::get()->getSettingValue<bool>("full-ver-btn")) {
 			CCMenuItemSpriteExtra* editBtn = dynamic_cast<CCMenuItemSpriteExtra*>(mainMenu->getChildByID("editor-button"));
